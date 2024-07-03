6b544ed9de5094f8bbec102553dc0a392f19bfb0
wj5495831 <734906926@qq.com>
2024-07-03 17:29:00 +0800

    正式服域名

--- vite.config.js
+++ vite.config.js
@@ -4,40 +4,41 @@ import vue from '@vitejs/plugin-vue2';
 import WindiCSS from 'vite-plugin-windicss';
 
 export default defineConfig({
-	plugins: [vue(), WindiCSS()],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
-		}
-	},
-	build: {
-		minify: 'terser', // 必须开启：使用terserOptions才有效果
-		terserOptions: {
-			compress: {
-				//生产环境时移除console
-				drop_console: true,
-				drop_debugger: true
-			}
-		}
-	},
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://192.168.1.233:8014',
-				changeOrigin: true,
-			}
-			// '/avatars': {
-			// 	target: 'http://localhost:3333',
-			// 	changeOrigin: true
-			// },
-			// '/pictures': {
-			// 	target: 'http://localhost:3333',
-			// 	changeOrigin: true
-			// },
-			// '/logos': {
-			// 	target: 'http://localhost:3333',
-			// 	changeOrigin: true
-			// }
-		}
-	}
+    plugins: [vue(), WindiCSS()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src',
+                import.meta.url))
+        }
+    },
+    build: {
+        minify: 'terser', // 必须开启：使用terserOptions才有效果
+        terserOptions: {
+            compress: {
+                //生产环境时移除console
+                drop_console: true,
+                drop_debugger: true
+            }
+        }
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://39.100.28.20:10001',
+                changeOrigin: true,
+            }
+            // '/avatars': {
+            // 	target: 'http://localhost:3333',
+            // 	changeOrigin: true
+            // },
+            // '/pictures': {
+            // 	target: 'http://localhost:3333',
+            // 	changeOrigin: true
+            // },
+            // '/logos': {
+            // 	target: 'http://localhost:3333',
+            // 	changeOrigin: true
+            // }
+        }
+    }
 });
